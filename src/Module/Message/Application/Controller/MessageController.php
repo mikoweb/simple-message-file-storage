bc185024d94788136a458f6e98694b77d87e1a72
Rafał Mikołajun <root@rmweb.pl>
2023-09-08 00:44:01 +0200

    add endpoint to get single message

--- src/Module/Message/Application/Controller/MessageController.php
+++ src/Module/Message/Application/Controller/MessageController.php
@@ -3,11 +3,14 @@
 namespace App\Module\Message\Application\Controller;
 
 use App\Module\Message\Application\Form\MessageSortFormType;
+use App\Module\Message\Infrastructure\Query\FindMessageQuery;
 use App\Module\Message\Infrastructure\Query\GetAllMessagesQuery;
 use App\Module\Message\Infrastructure\Query\SortMessagesQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Uid\Uuid;
 
 class MessageController extends AbstractController
 {
@@ -26,5 +29,16 @@ class MessageController extends AbstractController
         }
 
         return $this->json($messages->toArray());
+    }
+
+    public function getMessage(Uuid $id, FindMessageQuery $findMessageQuery): JsonResponse
+    {
+        $message = $findMessageQuery->find($id);
+
+        if (is_null($message)) {
+            return $this->json('Not found message', Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($message);
     }
 }
